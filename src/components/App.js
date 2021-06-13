592e5f4734be21b5ba9a173f84d76e5a0dea1b4f
Mohd Osama Abdullah <osama.abd10@gmail.com>
2021-06-14 00:09:59 +0530

    few changes
    few changes

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 28KvioApYfBa0p5gCkigM4f0Z41itduHvN9C-TNLKUw",
+      },
+    });
+
+    console.log(response.data.results);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
